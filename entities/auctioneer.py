fe8688f06ea16808b89cbda232addfe6e21dd00d
jantiend <j.g.dekker@hotmail.com>
2017-10-20 18:37:30 +0200

    Update auctioneer.py
    Please let me know if I handle things not as they're supposed to, this is the first time I've written code that someone else will see. ;) This is also the first time I had to work with multiple files, and therefore I only edited this one for now. I have some practical questions, but I'll ask on Monday.
    
    The buyers/sellers need a code. I was thinking of (a) adding this to the namedtuples in the buyers/sellers file so they also get a code instantly instead of at the auctioneer process, or (b) let the auctioneer transform the list of namedtuples to a dictionary (keys = buyer/seller codes, values = tuples). Didn't do any of them yet, since I wanted to discuss this first.
    
    The code doesn't yet include the complications mentioned by Nick, such as a new auction when a seller drops out of the auction (because of time and not knowing yet whether the current code suffices).
    
    --> Sandholm, T. (2002). eMediator: A next generation electronic commerce server.
            Computational Intelligence, 18(4), 656-676.
    
    Based on the Sandholm (2002) article, Middle Price 50:50 is best suited for determining
    the prices in double auctions. This also happens to be a simple way of determining the
    auction winners.
    Assumptions:
    - Demand is higher than supply (as discussed on Monday);
    - Multiple units of 1 (homogeneous) item;
    - All winners pay/get the same price.
    I found schemes that were way more interesting (especially when keeping the relationship
    with the other LCfP projects in mind), but it didn't fit the assumptions of an auction for
    multiple units of a single item in double sealed-bid auctions.
    
    To keep it simple (for now), I followed these steps:
    (1) Calculate Qs
    (2) Calculate median Ps
    (3) Calculate Qb
    (2) Deleted the lowest bids from the buyers, so Qs = Qb
    (3) Calculate median Pb
    (4) Calculate auction price (average of Ps and Pb)
    (5) Distribute the items to the highest bidders
    
    I tested the function with this data:
        Bid = namedtuple('Bid', ['action', 'quantity', 'item_price'])
        
        buyers = [Bid("buy", 200, 0.45),
                  Bid("buy", 250, 0.60),
                  Bid("buy", 400, 0.57),
                  Bid("buy", 150, 0.64)]
        
        sellers = [Bid("sell", 100, 0.80),
                   Bid("sell", 80, 0.90),
                   Bid("sell", 120, 0.79),
                   Bid("sell", 100, 0.88),
                   Bid("sell", 90, 0.84),
                   Bid("sell", 50, 0.99),
                   Bid("sell", 60, 0.92)]
        
        Results:
            There are 600 items for sale.
            Total demand is 1000
            Median seller price: 0.84
            Median buyer price: 0.6
            Final auction price: 0.72
            Buyer 0 gets 150 units.
            Buyer 1 gets 250 units.
            Buyer 2 gets 200 units.
            Buyer 3 gets nothing.

--- entities/auctioneer.py
+++ entities/auctioneer.py
@@ -1,6 +1,9 @@
 from .buyer import Buyer
 from .seller import Seller
 from typing import List
+from operator import attrgetter
+from collections import namedtuple
+from statistics import median
 
 
 class Auctioneer(object):
@@ -13,7 +16,57 @@ class Auctioneer(object):
         """
         Auctions with the buyers and sellers provided to this Auctioneer.
         """
-        pass  # TODO
+
+        # Get Qs and Median Ps:
+        total_q_s = 0
+        auction_items = []
+
+        for m in sellers:
+            total_q_s += m[1]
+            for i in range(m[1]):
+                auction_items.append(m[2])
+
+        median_p_s = median(sorted(auction_items))
+
+        # Get Qb and Median Pb:
+        total_q_b = 0
+        auction_items_b = []
+
+        for n in buyers:
+            total_q_b += n[1]
+            for j in range(n[1]):
+                auction_items_b.append(n[2])
+
+        auction_items_b = sorted(auction_items_b, reverse=True)
+        median_p_b = median(auction_items_b[0:total_q_s])
+
+        # Determine auction price:
+        price = (median_p_s + median_p_b) / 2
+
+        print("There are %s items for sale." % total_q_s)
+        print("Total demand is %s" % total_q_b)
+        print("Median seller price: %s" % median_p_s)
+        print("Median buyer price: %s" % median_p_b)
+        print("Final auction price: %s" % price)
+
+        # (5) Distribution of the auction items:
+        """
+        To distribute to the buyers with the highest bids, I had to sort the list. However, this returns the index
+        of the sorted list, not the original index. Haven't fixed this yet, first wanted to discuss solution.
+        """
+        buyers = sorted(buyers, key=attrgetter('item_price'), reverse=True)
+        for n in buyers:
+            buyer_quantity = n[1]
+
+            buyer_code = buyers.index(n)
+            if total_q_s >= buyer_quantity:
+                print("Buyer %s gets %s units." % (buyer_code, buyer_quantity))
+                total_q_s -= buyer_quantity
+            elif total_q_s > 0:
+                print("Buyer %s gets %s units." % (buyer_code, total_q_s))
+                total_q_s = 0
+            else:
+                print("Buyer %s gets nothing." % buyer_code)
 
     def _broker_bids(self):
         """
